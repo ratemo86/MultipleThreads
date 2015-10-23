d25811c9c7b8c087e55cef207c47f4fb1b7c0cbc
ratemo86 <jabuya@yahoo.com>
2015-10-23 15:15:56 -0400

    My First commit

---
+++ MultipleThreads/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultipleThreads
+{
+    class Program
+    {
+        delegate void ThreadDelegate(Thread thread);
+ 
+        //Shared by threads created by the main thread
+        static int c = 0;
+        private static void DisplayThreadId(Thread thread)
+
+        {
+            thread.Start();
+            
+            Console.WriteLine("Thread number: " + thread.ManagedThreadId + " has started.");
+            thread.Abort();
+           
+
+        }
+        private static void MyCallBack(IAsyncResult asychResult)
+        {
+
+            ThreadDelegate t = ((ThreadDelegate)asychResult.AsyncState);
+            t.EndInvoke(asychResult);
+            asychResult.AsyncWaitHandle.Close();
+          
+
+            Interlocked.Decrement(ref c);
+        }
+        
+        static void Main(string[] args)
+        {
+           
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadDelegate myDelegate = DisplayThreadId;
+                // ThreadDelegate myDel = DisplayThreadId(String threadId); 
+                Interlocked.Increment(ref c);
+                Thread thread = new Thread(() => { });
+                Console.WriteLine("Created thread number  " + c);
+
+                IAsyncResult res = myDelegate.BeginInvoke(thread, new AsyncCallback(MyCallBack), myDelegate);
+                
+                
+                // 
+            }
+            while (c > 0)
+            {
+                Thread.Sleep(1000);
+            }
+            Thread.CurrentThread.Join();
+            Console.ReadKey();
+        }
+    }
+}
